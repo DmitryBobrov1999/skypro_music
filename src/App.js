cc6618194342d32774f3c2b2681f760930d80316
Travel1999 <bobrovdmitry01@mail.ru>
2023-08-10 15:13:15 +0300

    added first components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreateSignInPage from './components/signin/CreateSigninPage.js';
+import Home from './components/Home/Home.js';
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				Здесь скоро будет Skypro.Music
-			</header>
+			<Router>
+				
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='/signin' element={<CreateSignInPage />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
