859ecff0495f6f686e72b356dc6d4f51d984fef7
Travel1999 <bobrovdmitry01@mail.ru>
2023-09-15 10:41:02 +0300

    hw6_3

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 export const getTracks = async () => {
-	const response = await fetch('https://painassasin.online/catalog/track/all/');
+	const response = await fetch(
+		'https://skypro-music-api.skyeng.tech/catalog/track/all/'
+	);
 
 	if (!response.ok) {
 		throw new Error('Не удалось загрузить плейлист, попробуйте позже');
